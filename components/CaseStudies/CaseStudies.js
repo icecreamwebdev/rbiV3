ff308bdc4533656bffda4e0d359d41bd029f1588
icecreamwebdev <gregorcassidy@gregors-mbp.home>
2022-03-20 14:17:43 +0000

    spacing on desktop

--- components/CaseStudies/CaseStudies.js
+++ components/CaseStudies/CaseStudies.js
@@ -22,7 +22,7 @@ Check out some of our favourites below.</p>
 
 {/* start of Video Section  */}
 
-            <div className='w-full flex p-2  md:flex-row flex-col bg-alt mb-6 md:mb-0'>
+            <div className='w-full flex p-2  md:flex-row flex-col bg-alt mb-6 '>
 
 
                 <div className='md:w-7/12 w-full h-full relative '>
@@ -62,7 +62,7 @@ Check out some of our favourites below.</p>
 
 
                
-            <div className='w-full flex md:flex-row-reverse flex-col p-2 bg-white '>
+            <div className='w-full flex md:flex-row-reverse flex-col p-2 bg-white mb-6 '>
 
 
 <div className='md:w-7/12 h-full relative '>
@@ -100,6 +100,166 @@ Check out some of our favourites below.</p>
 
 {/* end of double video section  */}
 
+
+{/* start of Video Section  */}
+
+<div className='w-full flex p-2  md:flex-row flex-col bg-alt mb-6 '>
+
+
+<div className='md:w-7/12 w-full h-full relative '>
+
+    <YoutubeEmbedd id='rBKJe7JtVu4' />
+
+    </div>
+
+    <div className='md:w-5/12 h-full flex flex-col items-center justify-center text-white p-10  bg-primary '>
+
+        <h2 className='flex text-white mb-3 uppercase items-center text-xl justify-center'>CLIENT: <span className='flex items-center pl-2'><Image src='/brands/brand10.png' width='80' height='18' objectFit='contain' /></span></h2>
+        <p className='font-semibold text-center text-sm'>Campaign goal: Increase brand visbility during Black Friday sales.</p>
+
+        <ul className='font-light text-xs mt-4 list-disc  pl-4 text-left w-full'>
+
+            <li>Platforms: Youtube (with instagram story accompaniment)</li>
+            <li>Ad style: pre-roll, mid-roll & instagram story</li>
+            <li>7 influencer activations</li>
+            <li>Campaign results: <br /><br /> <div className=' text-right'><ul >
+                
+                 <li>Youtube Views: <span className='text-alt'>1,385,000</span></li>
+                 <li>Likes: <span className='text-alt'>40.6K</span></li>
+                 <li>Comments: <span className='text-alt'>1,966</span></li>
+                 
+                 </ul>
+                 
+                 </div></li>
+        </ul>
+    </div>
+
+
+
+</div>
+
+
+{/* end of video section */}
+
+
+
+<div className='w-full flex md:flex-row-reverse flex-col p-2 bg-white mb-6 '>
+
+
+<div className='md:w-7/12 h-full relative '>
+
+<YoutubeEmbedd id='Mh9S1Gha8rw' />
+
+</div>
+
+<div className='md:w-5/12 h-full flex flex-col items-center justify-center bg-primary text-white p-10 '>
+
+<h2 className='flex mb-3 uppercase items-center text-xl justify-center'>CLIENT: <span className='flex items-center pl-2'><Image src='/brands/brand2.png' width='50' height='25' objectFit='contain'  /></span></h2>
+<p className='font-semibold text-center text-tiny'>Campaign goal: Increase brand visbility during Black Friday sales.</p>
+
+<ul className='font-light text-xs mt-4 list-disc  pl-4 text-left w-full'>
+
+<li>Platforms: Youtube</li>
+<li>Ad style: up to 3 minute pre roll ad</li>
+<li>24 influencer activations</li>
+<li>Campaign results: <br /><br /> <div className=' text-right'><ul >
+
+ <li>Youtube Views: <span className='text-alt'>4,317,646</span></li>
+ <li>Likes: <span className='text-alt'>168K</span></li>
+ <li>Comments: <span className='text-alt'>10,212</span></li>
+ 
+ </ul>
+ 
+ </div></li>
+</ul>
+</div>
+
+
+
+</div>
+
+
+{/* end of double video section  */}
+
+{/* start of Video Section  */}
+
+<div className='w-full flex p-2  md:flex-row flex-col bg-alt mb-6 '>
+
+
+<div className='md:w-7/12 w-full h-full relative '>
+
+    <YoutubeEmbedd id='rBKJe7JtVu4' />
+
+    </div>
+
+    <div className='md:w-5/12 h-full flex flex-col items-center justify-center text-white p-10  bg-primary '>
+
+        <h2 className='flex text-white mb-3 uppercase items-center text-xl justify-center'>CLIENT: <span className='flex items-center pl-2'><Image src='/brands/brand10.png' width='80' height='18' objectFit='contain' /></span></h2>
+        <p className='font-semibold text-center text-sm'>Campaign goal: Increase brand visbility during Black Friday sales.</p>
+
+        <ul className='font-light text-xs mt-4 list-disc  pl-4 text-left w-full'>
+
+            <li>Platforms: Youtube (with instagram story accompaniment)</li>
+            <li>Ad style: pre-roll, mid-roll & instagram story</li>
+            <li>7 influencer activations</li>
+            <li>Campaign results: <br /><br /> <div className=' text-right'><ul >
+                
+                 <li>Youtube Views: <span className='text-alt'>1,385,000</span></li>
+                 <li>Likes: <span className='text-alt'>40.6K</span></li>
+                 <li>Comments: <span className='text-alt'>1,966</span></li>
+                 
+                 </ul>
+                 
+                 </div></li>
+        </ul>
+    </div>
+
+
+
+</div>
+
+
+{/* end of video section */}
+
+
+
+<div className='w-full flex md:flex-row-reverse flex-col p-2 bg-white mb-6 '>
+
+
+<div className='md:w-7/12 h-full relative '>
+
+<YoutubeEmbedd id='Mh9S1Gha8rw' />
+
+</div>
+
+<div className='md:w-5/12 h-full flex flex-col items-center justify-center bg-primary text-white p-10 '>
+
+<h2 className='flex mb-3 uppercase items-center text-xl justify-center'>CLIENT: <span className='flex items-center pl-2'><Image src='/brands/brand2.png' width='50' height='25' objectFit='contain'  /></span></h2>
+<p className='font-semibold text-center text-tiny'>Campaign goal: Increase brand visbility during Black Friday sales.</p>
+
+<ul className='font-light text-xs mt-4 list-disc  pl-4 text-left w-full'>
+
+<li>Platforms: Youtube</li>
+<li>Ad style: up to 3 minute pre roll ad</li>
+<li>24 influencer activations</li>
+<li>Campaign results: <br /><br /> <div className=' text-right'><ul >
+
+ <li>Youtube Views: <span className='text-alt'>4,317,646</span></li>
+ <li>Likes: <span className='text-alt'>168K</span></li>
+ <li>Comments: <span className='text-alt'>10,212</span></li>
+ 
+ </ul>
+ 
+ </div></li>
+</ul>
+</div>
+
+
+
+</div>
+
+
+{/* end of double video section  */}
 
 
 
