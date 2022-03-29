9a223f0e3eaa8d551e604772ebc5c1aee6360bdb
icecreamwebdev <gregorcassidy@gregors-mbp.home>
2022-03-29 16:16:32 +0100

    add social buttons

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -3,12 +3,18 @@ import groq from "groq";
 import imageUrlBuilder from '@sanity/image-url'
 import {PortableText} from '@portabletext/react'
 import Footer from '../../components/Footer/Footer';
- 
+import { EmailIcon, EmailShareButton, FacebookShareButton, PinterestIcon, PinterestShareButton, RedditIcon, RedditShareButton, TwitterIcon, TwitterShareButton, WhatsappIcon, WhatsappShareButton } from "react-share";
+import { FacebookIcon } from "react-share";
+import { Router, useRouter } from "next/router";
+
 
 
 function urlFor (source) {
   return imageUrlBuilder(client).image(source)
 }
+
+
+
 
 const ptComponents = {
   types: {
@@ -33,6 +39,11 @@ const ptComponents = {
 const Post = ({post}) => {
 
 
+  const route = useRouter();
+  const titleSlug = route.query.slug;
+
+
+
   if (!post){
     return null;
   }
@@ -44,16 +55,20 @@ const Post = ({post}) => {
     authorImage,
     mainImage,
     _createdAt,
-    body = []
+    body = [],
   } = post
+
+  const shareUrl = 'https://rbiv3-cgdruxdon-icecreamwebdev.vercel.app/blog/' + titleSlug
+
   return (
     <>
     <div className='blog pb-12 p-7 pt-0'>
     <div className='max-w-2xl w-full justify-center md:mt-10 mt-7 m-auto xl:p-0 pb-10'>
     <article>
-      <h1 className='title'>{title}</h1>
-      <div className='flex mt-7 mb-5 items-center'>
+      <span className='md:text-3xl text-2xl font-extrabold'>{title}</span>
+      <div className='flex-col flex md:flex-row mt-7 mb-5 md:items-center justify-between'>
 
+    <div className='flex items-center'>
       {authorImage && (
         <div>
           <img className='rounded-full'
@@ -70,14 +85,67 @@ const Post = ({post}) => {
       <span className='font-base text-sm'>By {name}</span>
       <span className='text-xs text-gray-400 font-light'>{new Date(_createdAt).toDateString()}</span>
       </div>
+      </div>
       {/* {categories && (
         <ul>
           Posted in
           {categories.map(category => <li key={category}>{category}</li>)}
         </ul>
       )} */}
-      
-      </div>
+      <div className="flex space-x-1.5 mt-5">
+          <FacebookShareButton
+            url={shareUrl}
+            quote={title}
+            className="hover:opacity-80"
+          >
+            <FacebookIcon size={25} round />
+          </FacebookShareButton>
+
+          <TwitterShareButton
+            url={shareUrl}
+            quote={title}
+            className=""
+          >
+            <TwitterIcon size={25} round />
+          </TwitterShareButton>
+
+          <PinterestShareButton
+            url={shareUrl}
+            quote={title}
+            className=""
+          >
+            <PinterestIcon size={25} round />
+          </PinterestShareButton>
+
+          <WhatsappShareButton
+            url={shareUrl}
+            quote={title}
+            className=""
+          >
+            <WhatsappIcon size={25} round />
+          </WhatsappShareButton>
+
+          <RedditShareButton
+            url={shareUrl}
+            quote={title}
+            className=""
+          >
+            <RedditIcon size={25} round />
+          </RedditShareButton>
+
+          <EmailShareButton
+            url={shareUrl}
+            quote={title}
+            className=""
+          >
+            <EmailIcon size={25} round />
+          </EmailShareButton>
+
+         
+</div>
+</div>
+
+
 
 <div className='heroImage md:h-[450px] sm:h-[300px] h-[240px]'>
 <img
