3478c567e3394d95b3b04246e049a1999b99ec77
icecreamwebdev <66398783+icecreamwebdev@users.noreply.github.com>
2025-03-19 09:48:44 +0000

    Update Policies.js

--- components/Policies/Policies.js
+++ components/Policies/Policies.js
@@ -5,7 +5,7 @@ const Policies = () => {
   return (
     <div className="bg-[#3c3d3e] opacity-90 w-full flex  items-center border-b-4 border-white justify-center">
       <div className="max-w-2xl w-full md:flex-row w-full px-7 py-5 flex flex-col justify-between">
-        <p className="text-white text-center">RBI Media Limited | 2024</p>
+        <p className="text-white text-center">RBI Media Limited | 2025</p>
 
         <ul className="flex text-gray-400 font-light pt-3 md:pt-0 justify-center font-light text-sm">
           <li className="pl-4">
